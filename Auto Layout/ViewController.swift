147ea10905e8b4849514b2189a63940c51defa33
Fritola <fritola23@gmail.com>
2020-06-23 22:49:06 -0300

    Initial Commit

---
+++ Auto Layout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Auto Layout
+//
+//  Created by Gustavo Fritola on 23/06/20.
+//  Copyright © 2020 Gustavo Fritola. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
